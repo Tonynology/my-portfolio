5f7d7bf602ad091938533ac0c3a8f2b36cb05246
Tonynology <tonycontinue@gamil.com>
2020-12-13 18:36:57 -0800

    modify route issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,18 @@ import Education from './Education';
 
 const Pages = ({ user }) => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route exact path="/my-portfolio/">
+        <Route exact path="/">
           <Me user={user} />
         </Route>
-        <Route path="/my-portfolio/projects">
+        <Route path="/projects">
           <Projects user={user} />
         </Route>
-        <Route path="/my-portfolio/work">
+        <Route path="/work">
           <Work user={user} />
         </Route>
-        <Route path="/my-portfolio/education">
+        <Route path="/education">
           <Education user={user} />
         </Route>
       </Switch>
